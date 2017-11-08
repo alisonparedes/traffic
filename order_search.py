1e70964a32151eccadc0360ca7efe8520fa2f0b2
alison <ap2053@wildcats.unh.edu>
2017-11-08 16:47:08 -0500

    Tries ordering states

---
+++ order_search.py
@@ -0,0 +1,19 @@
+import state
+import transition
+import copy
+import time
+import random
+import rta_star
+import itertools
+
+if __name__ == "__main__":
+    current_queues, current_intersections, goal = state.init_problem()
+    print(current_intersections)
+    #start_time = time.clock()
+
+
+    #end_time = time.clock()-start_time
+    #print(end_time)
+
+
+
