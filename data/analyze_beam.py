3cf5e94502eb56eda54039432c3effa38ecde6d5
Alison Paredes <ap2053@wildcats.unh.edu>
2018-01-10 11:53:01 -0500

    Merged from beam branch

--- data/analyze_beam.py
+++ data/analyze_beam.py
@@ -12,9 +12,13 @@ sns.pairplot(bfs_d1)
 beam_d2["bfs_d1"] = bfs_d1["ex_time"]
 beam_d2["bfs_d1"]
 
-(sns.jointplot(x="ex_time",
+both = (sns.jointplot(x="ex_time",
                y="bfs_d1",
                data=beam_d2,
                xlim=(1000, 2400),
-               ylim=(1000, 2400),
-               kind="reg")).set_axis_labels("beam_d2", "bfs_d1")
+               ylim=(1000, 2400))).set_axis_labels("beam_d2", "bfs_d1")
+
+x0, x1 = both.ax_joint.get_xlim()
+y0, y1 = both.ax_joint.get_ylim()
+
+both.ax_joint.plot([x0, x1], [y0, y1], 'b-')
